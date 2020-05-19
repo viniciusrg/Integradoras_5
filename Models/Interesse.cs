a409bc56344ce8f448f365890c4f144177bbf027
Joshuanz <51347045+Joshuanz@users.noreply.github.com>
2020-05-18 22:36:23 -0300

    Interesse
    nada

---
+++ Models/Interesse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaoLendario.Models
+{
+    public class Interesse: Adotante
+    {
+        private DateTime data;
+        private Boolean adotado;
+
+        public DateTime Data { get; set; }
+        public Boolean Adotado { get; set; }
+    }
+}
