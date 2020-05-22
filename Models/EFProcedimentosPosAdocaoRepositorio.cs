bbc1dc3fbb38a57ec0c9537667b1be62475c459a
Darth98 <whalyssonaguiar@hotmail.com>
2020-05-21 21:59:59 -0300

    Parte 1.a

--- Models/EFProcedimentosPosAdocaoRepositorio.cs
+++ Models/EFProcedimentosPosAdocaoRepositorio.cs
@@ -8,6 +8,36 @@ namespace CaoLendario.Models
 {
     public class EFProcedimentosPosAdocaoRepositorio : IProcedimentosPosAdocaoRepositorio
     {
+        private ApplicationDbContext context;
 
+        public EFProcedimentosPosAdocaoRepositorio(ApplicationDbContext ctx)
+        {
+            context = ctx;
+        }
+        public IQueryable<ProcedimentosPosAdocao> context.ProcedimentosPosAdocao;
+
+        public void Create(ProcedimentosPosAdocao procedimentosPosAdocao)
+        {
+            context.Add(procedimentosPosAdocao);
+            context.SaveChanges();
+        }
+
+        public void Delete(ProcedimentosPosAdocao procedimentosPosAdocao)
+        {
+            context.Remove(procedimentosPosAdocao);
+            context.SaveChanges();
+        }
+
+        public void Edit(ProcedimentosPosAdocao procedimentosPosAdocao)
+        {
+            context.Entry(procedimentosPosAdocao).State = EntityState.Modified;
+            context.SaveChanges();
+        }
+
+        public ProcedimentosPosAdocao ObterProcedimentosPosAdocao(int id)
+        {
+            var procedimentosposadocao = context.ProcedimentosPosAdocao.Find(id);
+            return procedimentosposadocao;
+        }
     }
 }
