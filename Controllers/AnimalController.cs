1604938392164bfbcc4147a145a8690bebb590d1
Darth98 <whalyssonaguiar@hotmail.com>
2020-05-21 21:48:58 -0300

    Parte 1

--- Controllers/AnimalController.cs
+++ Controllers/AnimalController.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CaoLendario.Controller
+namespace CaoLendario.Controllers
 {
-    public class AnimalController
+    public class AnimalControllers
     {
     }
 }
