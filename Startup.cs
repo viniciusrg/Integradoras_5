a2963722a7a686209b12f03c655f96a234968dd7
Celso de Ávila Ramos <37908497+celsodeavila@users.noreply.github.com>
2020-05-25 18:31:38 -0300

    startup

--- Startup.cs
+++ Startup.cs
@@ -1,60 +1,60 @@
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using CaoLendario.Models;
+using SportStore.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.SqlServer;
 
-namespace CaoLendario
+
+namespace SportStore
 {
     public class Startup
     {
         //o método contrutor recebe a configuração de dados carregada de appsettings.json
-        public Startup(IConfiguration configuration) => Configuration = configuration;
+        public Startup(IConfiguration configuration) => 
+            Configuration = configuration;
         public IConfiguration Configuration { get; }
-        //Este metodo é usado para preparar os objetos compartilhados que serão usados
-        //durante a aplicação.Este compartilhamento é chamdo de Injeção de
-        //Dependência.Por exemplo, quando um banco de dados depende das definições
-        //de um modelo para ser criado.
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            //configura os serviços oferecidos pela Entity Framework Core para a classe contexto do banco de dados
-            //services.AddDbContext<ApplicationDbContext>(options =>
-            //options.UseSqlServer(Configuration["Data:SportStoreProdutos:ConnectionString"]));
-            //Associa o Repositório EFProdutoRepositorio à interface IProdutoRepositorio
-            //services.AddTransient<IProdutoRepositorio, EFProdutoRepositorio>();
-            //services.AddTransient<IFabricanteRepositorio, EFFabricanteRepositorio>();
-            //habilita o ASP.NET Core
-            services.AddMvc();
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(
+                Configuration["Data:SportStoreProdutos:ConnectionString"]));
+            services.AddTransient<IProdutoRepositorio, EFProdutoRepositorio>();
+            services.AddTransient<IFabricanteRepositorio, EFFabricanteRepositorio>();
+            services.AddMvc();                        
         }
 
-        //Este metodo é usado para preparar os componentes que recebem e processam
-        //requisições HTTP.
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
             if (env.IsDevelopment())
             {
-                // Mostra detalhes das exceções que ocorrem na aplicação.
                 app.UseDeveloperExceptionPage();
             }
-            // Adiciona uma mensagem de resposta HTTP, como por exemplo 404 Not Found Responses.
             app.UseStatusCodePages();
-            // habilita o suporte para arquivos contidos em wwwroot, como css e javascript
             app.UseStaticFiles();
             app.UseRouting();
 
-            //definimos que a rota default será definida pela
-            //invocação da action List que está no controller Produto.
+     
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                name: "default",
-                pattern: "{controller}/{action}/{id?}",
-                defaults: new { controller = "Animal", action = "List" });
+             endpoints.MapControllerRoute(
+             name: "default",
+             pattern: "{controller}/{action}/{id?}",
+             defaults: new { controller = "Produto", action = "List" });
             });
-            //ativa a população do banco de dados quando a aplicação for iniciada          
-            //SeedData.EnsurePopulated(app);
+            SeedData.EnsurePopulated(app);
         }
     }
 }
