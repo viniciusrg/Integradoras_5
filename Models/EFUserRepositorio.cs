c7e1d717deda3e20ee15ea38d0f9cd40ce93dc67
Miller <millerfernandes53@gmail.com>
2020-05-28 22:12:27 -0300

    Adição Repositorio e Interface

---
+++ Models/EFUserRepositorio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaoLendario.Models
+{
+	public class EFUserRepositorio
+	{
+        private ApplicationDbContext context;
+
+        public EFUserRepositorio(ApplicationDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IQueryable<User> Users => context.Adotantes;
+
+
+    }
+}
---
+++ Models/IUserRepositorio.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaoLendario.Models
+{
+	interface IUserRepositorio
+	{
+	}
+}
