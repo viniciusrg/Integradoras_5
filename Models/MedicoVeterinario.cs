db7e66cfc29590de5ee9409a6f7086221913be2c
carloseduardo9959 <50346234+carloseduardo9959@users.noreply.github.com>
2020-06-04 21:36:19 -0300

    Update MedicoVeterinario.cs

--- Models/MedicoVeterinario.cs
+++ Models/MedicoVeterinario.cs
@@ -5,7 +5,12 @@ using System.Threading.Tasks;
 
 namespace CaoLendario.Models
 {
-	public class MedicoVeterinario
+	public class MedicoVeterinario: User
 	{
-	}
+        public int MedicoID { get; set; }
+        public string CRMV { get; set; }
+        public DateTime Data { get; set; }
+
+        public virtual ICollection<ProcedimentosPreAdocao> ProcedimentosPreAdocao { get; set; }
+    }
 }
