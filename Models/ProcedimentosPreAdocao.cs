c32afde8db7a79e8ed4b5bb5075035202db49b4b
AdrielRA <adrieldeveloper@hotmail.com>
2020-06-08 21:23:37 -0300

    change_pre_pos

--- Models/ProcedimentosPreAdocao.cs
+++ Models/ProcedimentosPreAdocao.cs
@@ -7,11 +7,8 @@ namespace CaoLendario.Models
 {
     public class ProcedimentosPreAdocao
     {
-        private string descricao;
-        private DateTime data;
+        private string descricao { get; set; }
+        private DateTime data { get; set; }
         public int ProcedimentosPreAdocaoID { get; set; }
-
-        public string Descricao { get => descricao; set => descricao = value; }
-        public DateTime Data { get => data; set => data = value; }
     }
 }
