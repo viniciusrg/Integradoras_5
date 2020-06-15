7518624450f73de7b7f975d2cefcfb6399279e0f
Celso de Ávila Ramos <37908497+celsodeavila@users.noreply.github.com>
2020-06-15 18:00:45 -0300

    dev updt

--- Models/MedicoVeterinario.cs
+++ Models/MedicoVeterinario.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace CaoLendario.Models
 {
-	public class MedicoVeterinario: User
+	public class MedicoVeterinario : User
 	{
         public string CRMV { get; set; }
         public DateTime Data { get; set; }
