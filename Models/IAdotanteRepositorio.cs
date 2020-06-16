2ce9c3dbf5e047876d17c95010611d479eb0d2c5
Miller <millerfernandes53@gmail.com>
2020-06-15 22:35:50 -0300

    Adotante Correções 2

--- Models/IAdotanteRepositorio.cs
+++ Models/IAdotanteRepositorio.cs
@@ -8,5 +8,11 @@ namespace CaoLendario.Models
 	interface IAdotanteRepositorio
 	{
 		IQueryable<Adotante> Adotantes { get; }
+		public Adotante ObterAdotante(int id);
+		public void Create(Adotante adotante);
+		public void Edit(Adotante adotante);
+		public void Delete(Adotante adotante);
+
+
 	}
 }
